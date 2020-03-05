75de872d53da0c27dcbf0aaeb8f01c65a92d2a3c
ziyadshaikh013 <ziyadshaikh013@gmail.com>
2020-03-05 13:01:42 +0530

    Updates

---
+++ precache-manifest.35ba6891ba6252f4f0e8b9dc2d0c450f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326a2ca36a43a34021678584d0f6ed97",
+    "url": "/blog-app/index.html"
+  },
+  {
+    "revision": "335b965919903f982123",
+    "url": "/blog-app/static/css/main.70ba5bdf.chunk.css"
+  },
+  {
+    "revision": "5001c783942e92dab5dc",
+    "url": "/blog-app/static/js/2.d03d4328.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/blog-app/static/js/2.d03d4328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "335b965919903f982123",
+    "url": "/blog-app/static/js/main.f31e027c.chunk.js"
+  },
+  {
+    "revision": "905efe01fb91f44400b0",
+    "url": "/blog-app/static/js/runtime-main.cd64f752.js"
+  }
+]);
